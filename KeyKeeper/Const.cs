a94e42ff092f6b955d1d05ded4e57b4265f842be
Guki <guki@tbp-laptop.(none)>
2012-03-16 13:20:43 +0300

    тестовый коммит
    добавлен класс const с константами

---
+++ KeyKeeper/Const.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace KeyKeeper
+{
+	public static class Const
+	{
+		public const uint OPERATION_WORK_IN = 1;
+		public const uint OPERATION_WORK_OUT = 2;
+		public const uint OPERATION_ITEM_GET = 3;
+		public const uint OPERATION_ITEM_PUT = 4;
+	}
+}
+
--- KeyKeeper/dbHelper.cs
+++ KeyKeeper/dbHelper.cs
@@ -39,7 +39,8 @@ namespace KeyKeeper
 			var list = new List<Worker>();
 			
 			IDataReader reader = dbConnector.getdbAcces().readbd(
-				"select *,concat_ws(' ',f,i,o) as fio, concat(f,' ',left(i,1),'. ',left(o,1),'.') as shortfio from workers;");
+				@"select *,concat_ws(' ',f,i,o) as fio, concat(f,' ',left(i,1),'. ',left(o,1),'.') as shortfio 
+					from workers;");
 			try
 			{
 				while(reader.Read())
@@ -64,9 +65,9 @@ namespace KeyKeeper
 			var list = new List<Worker>();
 			
 			IDataReader reader = dbConnector.getdbAcces().readbd(
-				@"select w.*,concat_ws(' ',f,i,o) as fio, concat(f,' ',left(i,1),'. ',left(o,1),'.') as shortfio
+				string.Format(@"select w.*,concat_ws(' ',f,i,o) as fio, concat(f,' ',left(i,1),'. ',left(o,1),'.') as shortfio
 				from journal j join workers w on j.worker_id=w.id
-				where isnull(j.stamp_end) and j.operation_id=1");
+				where isnull(j.stamp_end) and j.operation_id={0}",Const::OPERATION_WORK_IN));
 			try
 			{
 				while(reader.Read())
@@ -91,93 +92,6 @@ namespace KeyKeeper
 			Utils.showMessageInfo("ee");
 			return 0; 
 		}
-		
-		#region мусор
-		public static string getFIO(uint workerid)
-		{
-			string fio = null;
-			IDataReader reader = dbConnector.getdbAcces().readbd(
-				"select concat_ws(' ',f,i,o) as fio from workers where id='"+workerid+"';");
-			try
-			{
-				reader.Read();
-				fio = (string) reader["fio"];
-			}
-			catch(MySqlException ex)
-			{
-				Utils.showMessageError(ex.ToString());
-			}
-		
-			reader.Close();
-       		reader = null;
-		
-			return fio;	
-		}
-		
-		public static string getShortFIO(uint workerid)
-		{
-			string shortFIO = null;
-			IDataReader reader = dbConnector.getdbAcces().readbd(
-				"select concat(f,' ',left(i,1),'. ',left(o,1),'.') as fio from workers where id='"+workerid+"';");
-			try
-			{
-				reader.Read();
-				shortFIO = (string) reader["fio"];
-			}
-			catch(MySqlException ex)
-			{
-				Utils.showMessageError(ex.ToString());
-			}
-			
-			reader.Close();
-       		reader = null;
-			
-			return shortFIO;
-		}
-		
-		public static string getPhone(uint workerid)
-		{
-			string phone = null;
-			IDataReader reader = dbConnector.getdbAcces().readbd(
-				"SELECT phone FROM keykeeper.workers where id='"+workerid+"';");
-			try
-			{
-				reader.Read();
-				phone = (string) reader["phone"];
-			}
-			catch(MySqlException ex)
-			{
-				Utils.showMessageError(ex.ToString());
-			}
-			
-			reader.Close();
-       		reader = null;
-			
-			return phone;
-			
-		}
-		
-		public static uint getCode(uint workerid)
-		{
-			uint code = 0;
-			IDataReader reader = dbConnector.getdbAcces().readbd(
-				"SELECT code FROM keykeeper.workers where id='"+workerid+"';");
-			try
-			{
-				reader.Read();
-				code = (uint) reader["code"];
-			}
-			catch(MySqlException ex)
-			{
-				Utils.showMessageError(ex.ToString());
-			}
-			
-			reader.Close();
-       		reader = null;
-			
-			return code;
-		}
-			#endregion
 	}
 }
 
